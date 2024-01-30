6d96bf68714abe9a20629715f96acbddd7835d72
Samuel Morales <sammorales95@gmail.com>
2024-01-30 16:28:24 -0500

    Added imputation and dropping rows/columns

---
+++ src/cleaning.py
@@ -0,0 +1,33 @@
+import pandas as pd
+
+class DataCleaner:
+    def __init__(self, df):
+        """Initialize with a DataFrame"""
+        self.df = df
+
+
+    # --- Handle Missing Data ---
+    def drop_missing_rows(self):
+        """Drop rows with missing values"""
+        self.df.dropna(inplace=True)
+        return self.df
+
+    def drop_missing_columns(self, threshold=0.5):
+        """Drop columns with missing values above a specified threshold"""
+        self.df.dropna(thresh=threshold*len(self.df), axis=1, inplace=True)
+        return self.df
+    
+    def fill_missing_values(self, columns, strategy='mean'):
+        """Impute missing values in specified columns using specified strategy"""
+        for col in columns:
+            if strategy == 'mean':
+                self.df[col].fillna(self.df[col].mean(), inplace=True)
+            elif strategy == 'median':
+                self.df[col].fillna(self.df[col].median(), inplace=True)
+            elif strategy == 'mode':
+                self.df[col].fillna(self.df[col].mode()[0], inplace=True)
+            else:
+                print('Invalid imputation strategy')
+                return None
+
+    
